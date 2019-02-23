a34c93ad53a6ef1fd75949d3a602a754dfaf7262
CaprusWl <capruswl@gmail.com>
2019-02-23 17:51:47 +0800

    day19

---
+++ MaximunSubarray.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int max = nums[0];
+        if (nums.length == 0) {
+            return 0;
+        }
+        if (nums.length == 1) {
+            return nums[0];
+        }
+        
+        for (int i = 1; i < nums.length; i++) {
+            nums[i] = Math.max(nums[i], nums[i] + nums[i - 1]);
+            max = Math.max(max, nums[i]);
+        }
+        
+        return max;
+    }
+}
